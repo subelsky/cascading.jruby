dfdebd235a17b3057b82c829e36080d9ce5078c8
mwalker <matt.r.walker@gmail.com>
2011-08-15 11:50:29 -0500

    Be specific about testing ExprStub (now that all tests involve only it)

--- spec/expr_spec.rb
+++ spec/expr_spec.rb
@@ -1,6 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe Object do
+describe ExprStub do
   it 'should allow expr syntax' do
     test_assembly do
       insert 'foo' => 1, 'bar' => expr('offset:int')
