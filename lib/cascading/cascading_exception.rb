2908c2d54d3c229a87383c34a3a2629e44e86756
Matt Walker <mwalker@etsy.com>
2012-03-29 11:19:44 -0500

    Improve and clarify CascadingException such that requests for non-existent causes don't throw an exception; add tests

--- lib/cascading/cascading_exception.rb
+++ lib/cascading/cascading_exception.rb
@@ -5,13 +5,23 @@
 # underlying Janino expression problems.
 module Cascading
   class CascadingException < StandardError
+    attr_accessor :ne, :message, :depth
+
     def initialize(native_exception, message)
       @ne = native_exception
-      super("#{message}\n#{trace_causes(@ne, 1)}")
+      @message = message
+      trace, @depth = trace_causes(@ne, 1)
+      super("#{message}\n#{trace}")
     end
 
-    def cause(depth)
-      fetch_cause(@ne, depth)
+    # Fetch cause at depth.  If depth is not provided, root cause is returned.
+    def cause(depth = @depth)
+      if depth > @depth
+        warn "WARNING: Depth (#{depth}) greater than depth of cause stack (#{@depth}) requested"
+        nil
+      else
+        fetch_cause(@ne, depth)
+      end
     end
 
     private
@@ -22,14 +32,20 @@ module Cascading
     end
 
     def trace_causes(ne, depth)
-      return unless ne
+      return ['', depth - 1] unless ne
+
+      warn "ERROR: Exception does not respond to cause: #{ne}" unless ne.respond_to?(:cause)
+      cause_trace, cause_depth = trace_causes(ne.respond_to?(:cause) ? ne.cause : nil, depth + 1)
+
       trace = "Cause #{depth}: #{ne.respond_to?(:java_class) ? ne.java_class : ne.class}: #{ne}\n"
       if ne.respond_to?(:stack_trace)
         trace += "#{ne.stack_trace.map{ |e| "  at #{e.class_name}.#{e.method_name}(#{e.file_name}:#{e.line_number})" }.join("\n")}\n"
       elsif ne.respond_to?(:backtrace)
         trace += "  #{ne.backtrace.join("\n  ")}\n"
       end
-      trace += "#{trace_causes(ne.cause, depth + 1)}"
+      trace += cause_trace
+
+      [trace, cause_depth]
     end
   end
 end
---
+++ test/test_exceptions.rb
@@ -0,0 +1,41 @@
+require 'test/unit'
+require 'cascading'
+
+class TC_Exceptions < Test::Unit::TestCase
+  def test_cascading_exception
+    ne3 = java.lang.IllegalArgumentException.new('Root cause')
+    ne2 = Java::CascadingPipe::OperatorException.new(Java::CascadingPipe::Pipe.new('dummy'), 'Exception thrown by Cascading', ne3)
+    ne1 = Java::Cascading::CascadingException.new('Exception Cascading hands us', ne2)
+    e = CascadingException.new(ne1, 'cascading.jruby wrapper exception')
+
+    assert_equal ne1, e.ne
+    assert_equal 'cascading.jruby wrapper exception', e.message
+    assert_equal 3, e.depth
+
+
+    assert_equal ne1, e.cause(1)
+    assert_equal 'Exception Cascading hands us', e.cause(1).message
+
+    assert_equal ne2, e.cause(2)
+    # Cascading inserts Operator#to_s, here
+    assert_match /Exception thrown by Cascading/, e.cause(2).message
+
+    assert_equal ne3, e.cause(3)
+    assert_equal 'Root cause', e.cause(3).message
+
+    # Shallower than depth 1 is the first cause
+    (-5..0).each do |i|
+      assert_equal ne1, e.cause(i)
+      assert_equal 'Exception Cascading hands us', e.cause(i).message
+    end
+
+    # Deeper than the root cause is nil
+    (4..10).each do |i|
+      assert_nil e.cause(i)
+    end
+
+    # cause without depth returns root cause
+    assert_equal ne3, e.cause
+    assert_equal 'Root cause', e.cause.message
+  end
+end
