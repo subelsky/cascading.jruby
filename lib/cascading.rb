0fd56bfa4244fcf8c9b4ab0cca2415ef6d64d26e
Matt Walker <mwalker@etsy.com>
2013-01-30 07:03:24 -0600

    Revert "Remove unnecessary and complicated jar loading code that added dependency on environment variables at one time; just leave adding Cascading and Hadoop to the classpath up to the user and the build"
    This reverts commit 3a7ec133dc82b721a7ca9adbc69261b758838d86.

--- lib/cascading.rb
+++ lib/cascading.rb
@@ -3,7 +3,46 @@ require 'java'
 module Cascading
   # :stopdoc:
   VERSION = '0.0.10'
+  LIBPATH = ::File.expand_path(::File.dirname(__FILE__)) + ::File::SEPARATOR
+  PATH = ::File.dirname(LIBPATH) + ::File::SEPARATOR
+  CASCADING_HOME = ENV['CASCADING_HOME']
+  HADOOP_HOME = ENV['HADOOP_HOME']
+
+  # :startdoc:
+
+  # Returns the version string for the library.
+  #
+  def self.version
+    VERSION
+  end
+
+  # Returns the library path for the module. If any arguments are given,
+  # they will be joined to the end of the libray path using
+  # <tt>File.join</tt>.
+  #
+  def self.libpath( *args )
+    args.empty? ? LIBPATH : ::File.join(LIBPATH, args.flatten)
+  end
+
+  # Returns the lpath for the module. If any arguments are given,
+  # they will be joined to the end of the path using
+  # <tt>File.join</tt>.
+  #
+  def self.path( *args )
+    args.empty? ? PATH : ::File.join(PATH, args.flatten)
+  end
+
+  def self.require_all_jars(from = ::File.join(::File.dirname(__FILE__), "..", "jars"))
+    search_me = ::File.expand_path(
+        ::File.join(from, '**', '*.jar'))
+    Dir.glob(search_me).sort.each do |jar|
+      require jar
+    end
+  end
 end
+
+Cascading.require_all_jars(Cascading::HADOOP_HOME) if Cascading::HADOOP_HOME
+Cascading.require_all_jars(Cascading::CASCADING_HOME) if Cascading::CASCADING_HOME
 
 require 'cascading/assembly'
 require 'cascading/base'
